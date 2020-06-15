50bf754082070c017b2dc51d1c4e57098d23fb94
= <samuelgajardo@outlook.com>
2020-06-15 13:29:16 -0400

    prueba

---
+++ constants.php
@@ -0,0 +1,6 @@
+<?php
+define('RECEIVER_ID', 313698);
+define('SECRET', 'f4c0d221c20046c290f393504acc7f0ccf603f69');
+define('BASE_PATH', 'http://localhost/PagoKhipu/');
+define('RETURN_URL', BASE_PATH . 'finish.php');
+define('NOTIFY_URL', BASE_PATH . 'demo-notify-js.php');
