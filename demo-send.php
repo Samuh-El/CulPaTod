0d7ecd638e4965f2bed39ad8db69d00363393569
= <samuelgajardo@outlook.com>
2020-06-17 20:51:39 -0400

    kirby

--- demo-send.php
+++ demo-send.php
@@ -9,7 +9,7 @@ if (!filter_var($_REQUEST['email'], FILTER_VALIDATE_EMAIL)) {
 $configuration = new Khipu\Configuration();
 $configuration->setReceiverId(RECEIVER_ID);
 $configuration->setSecret(SECRET);
-// $configuration->setDebug(true);
+$configuration->setDebug(true);
 
 $client = new Khipu\ApiClient($configuration);
 $payments = new Khipu\Client\PaymentsApi($client);
@@ -17,16 +17,16 @@ $payments = new Khipu\Client\PaymentsApi($client);
 
 try {
     $opts = array(
-        "body" => "Este es un pago de pruebas para usar la biblioteca khipu.",
+        "body" => "Pago del ticket de evento",
         "bank_id" => $_REQUEST['bankId'],
         "payer_email" => $_REQUEST['email'],
         "return_url" => RETURN_URL,
         "notify_url" => NOTIFY_URL,
         "notify_api_version" => "1.3"
     );
-    $response = $payments->paymentsPost("Ejemplo de pago usando biblioteca Javascript de khipu" //Motivo de la compra
+    $response = $payments->paymentsPost("Tiket de evento" //Motivo de la compra
         , "CLP" //Moneda
-        , 100.0 //Monto
+        , 5000.0 //Monto
         , $opts );
 
     header('Location: process.php?id='. $response->getPaymentId().'&url='.$response->getPaymentUrl().'&ready_for_terminal='.$response->getReadyForTerminal());
