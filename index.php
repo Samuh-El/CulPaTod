aaa455038d2f35b215f4cce49cd05644c955e97a
= <samuelgajardo@outlook.com>
2020-06-17 21:59:48 -0400

    asjdh

--- index.php
+++ index.php
@@ -1,49 +1,118 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
+include('constants.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="images/favicon.ico">
 
-$configuration = new Khipu\Configuration();
-$configuration->setSecret('f4c0d221c20046c290f393504acc7f0ccf603f69');
-$configuration->setReceiverId('313698');
-$configuration->setPlatform('demo-client', '2.0');
-//$configuration->setDebug(true);
-$client = new Khipu\ApiClient($configuration);
-$payments = new Khipu\Client\PaymentsApi($client);
+    <title>Llena los datos para tu compra.</title>
 
-echo "<br>declaro variables";
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
-try {
-    echo "<br>entro al try";
-    $ops = array(
-        "notify_url" => "https://culturaparatodos.herokuapp.com/demo-notify-js.php",
-    );
-    echo "<br>antes response";
-    $response = $payments->paymentsPost('Pago de demo', 'CLP', 1000, $ops);
-    echo "<br>despues response";
+    <!-- Custom styles for this template -->
+    <link href="css/cover.css" rel="stylesheet">
 
-    echo "<br>PAYMENT_ID: " . $response->getPaymentId() . "\n";
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]>
+    <script src="js/ie8-responsive-file-warning.js"></script><![endif]-->
 
-} catch (Exception $e) {
-    echo "<br>entro al catch 1";
-    echo "<br>" . $e->getMessage();
-}
-////////////////////////
-$notificationToken = $_REQUEST['notification_token'];
-$client = new Khipu\ApiClient($configuration);
-$payments = new Khipu\Client\PaymentsApi($client);
-echo "<br>declaro variables 2<br>";
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
 
-try {
-    echo "<br>try 2<br>";
-    $response = $payments->paymentsGet($notificationToken);
-    echo "<br>paso response2<br>";
+<body>
 
-    print "PAYMENT_ID: " . $response->getPaymentId() . "\n";
-    print "TRANSACTION_ID: " . $response->getTransactionId() . "\n";
-    print "AMOUNT: " . $response->getAmount() . "\n";
-    print "CURRENCY: " . $response->getCurrency() . "\n";
-    print "STATUS: " . $response->getStatus() . "\n";
+<div class="site-wrapper">
 
-} catch (Exception $e) {
-    echo "<br>entro al catch2<br>";
-    echo $e->getMessage();
-}
+    <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+            <div class="masthead clearfix">
+                <div class="inner">
+                    <h3 class="masthead-brand">Comercio de ejemplo</h3>
+                    <ul class="nav masthead-nav">
+                        <li class="active"><a href="#">Demo</a></li>
+                        <li><a href="https://khipu.com/page/api">API Rest</a></li>
+                        <li><a href="https://github.com/khipu/lib-javascript">Biblioteca Javascript</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="inner cover">
+                <h1 class="cover-heading">Llena los datos para tu compra.</h1>
+
+                <form class="form-horizontal" role="form" action="demo-send.php" method="post">
+                    <div class="form-group <?php echo $_REQUEST['invalid'] ? 'has-error' : ''; ?>">
+                        <label for="email" class="col-sm-4 control-label">Ingresa tu correo electr&oacute;nico</label>
+
+                        <div class="col-sm-8">
+                            <input type="email" class="form-control" id="email" name="email" placeholder="mi@correo.cl">
+                        </div>
+
+                    </div>
+                    <div class="form-group">
+                        <label for="bankId" class="col-sm-4 control-label">Elige tu banco</label>
+
+                        <div class="col-sm-8">
+                            <select name="bankId" class="form-control" id="bankId">
+                                <?php
+                                $configuration = new Khipu\Configuration();
+                                $configuration->setReceiverId(RECEIVER_ID);
+                                $configuration->setSecret(SECRET);
+                                // $configuration->setDebug(true);
+
+                                $client = new Khipu\ApiClient($configuration);
+                                $banksApi = new Khipu\Client\BanksApi($client);
+
+                                try {
+                                    $response = $banksApi->banksGet();
+                                    foreach ($response->getBanks() as $bank) {
+                                        echo "<option value=\"" . $bank->getBankId() . "\">" . $bank->getName()
+                                            . "</option>";
+                                    }
+                                } catch (\Khipu\ApiException $e) {
+                                    echo print_r($e->getResponseBody(), TRUE);
+                                }
+                                ?>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-offset-4 col-sm-10">
+                            <button type="submit" class="btn btn-primary">Revisar orden y pagar</button>
+                        </div>
+                    </div>
+                </form>
+
+            </div>
+
+            <div class="mastfoot">
+                <div class="inner">
+                    <p>Cover template for <a href="http://getbootstrap.com">Bootstrap</a>, by <a
+                            href="https://twitter.com/mdo">@mdo</a>.</p>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/docs.min.js"></script>
+</body>
+</html>
