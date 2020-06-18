6d5556c2fb5d8e6a17a7ba398416b3dbe32bc525
= <samuelgajardo@outlook.com>
2020-06-17 21:42:54 -0400

    xd

--- index.php
+++ index.php
@@ -5,26 +5,26 @@ $configuration = new Khipu\Configuration();
 $configuration->setSecret('f4c0d221c20046c290f393504acc7f0ccf603f69');
 $configuration->setReceiverId('313698');
 $configuration->setPlatform('demo-client', '2.0');
-$configuration->setDebug(true);
+//$configuration->setDebug(true);
 $client = new Khipu\ApiClient($configuration);
 $payments = new Khipu\Client\PaymentsApi($client);
 
-echo "declaro variables";
+echo "<br>declaro variables";
 
 try {
-    echo "entro al try";
+    echo "<br>entro al try";
     $ops = array(
         "notify_url" => "https://culturaparatodos.herokuapp.com/",
-        "transaction_id" => "TX-1234",
     );
-    echo "antes response";
+    echo "<br>antes response";
     $response = $payments->paymentsPost('Pago de demo', 'CLP', 1000, $ops);
-    echo "despues response";
+    echo "<br>despues response";
 
-    echo "PAYMENT_ID: " . $response->getPaymentId() . "\n";
+    echo "<br>PAYMENT_ID: " . $response->getPaymentId() . "\n";
 
 } catch (Exception $e) {
-    echo $e->getMessage();
+    echo "<br>entro al catch 1";
+    echo "<br>" . $e->getMessage();
 }
 ////////////////////////
 $notificationToken = trim(file_get_contents("../NOTIFICATION_TOKEN"));
@@ -33,7 +33,7 @@ $payments = new Khipu\Client\PaymentsApi($client);
 echo "declaro variables 2";
 
 try {
-    echo "try 2";
+    echo "<br>try 2";
     $response = $payments->paymentsGet($notificationToken);
 
     print "PAYMENT_ID: " . $response->getPaymentId() . "\n";
@@ -43,5 +43,6 @@ try {
     print "STATUS: " . $response->getStatus() . "\n";
 
 } catch (Exception $e) {
+    echo "<br>entro al catch2";
     echo $e->getMessage();
 }
