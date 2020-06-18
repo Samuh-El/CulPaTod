085285c6ff7826037ca5901890411d01769952a8
= <samuelgajardo@outlook.com>
2020-06-17 20:18:27 -0400

    ajsd

---
+++ demo-notify-js.php
@@ -0,0 +1,47 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+include('constants.php');
+
+$api_version = 'api_version';  // Parámetro api_version
+$notification_token = $_REQUEST['notification_token']; //Parámetro notification_token
+$amount = 5000;
+
+
+try {
+    if ($api_version == '1.3') {
+        $configuration = new Khipu\Configuration();
+        $configuration->setSecret(SECRET);
+        $configuration->setReceiverId(RECEIVER_ID);
+        // $configuration->setDebug(true);
+
+        $client = new Khipu\ApiClient($configuration);
+        $payments = new Khipu\Client\PaymentsApi($client);
+
+        $response = $payments->paymentsGet($notification_token);
+
+
+        if ($response->getReceiverId() == RECEIVER_ID) {
+            if ($response->getStatus() == 'done' && $response->getAmount() == $amount) {
+                $headers = 'From: "Comercio de prueba" <no-responder@khipu.com>' . "\r\n";
+                $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+                $subject = 'La compra de prueba funciona';
+                $body = <<<EOF
+Hola<br/>
+<br/>
+<p>
+Recibes este correo pues el pago de prueba fue conciliado por khipu
+</p>
+
+EOF;
+                mail($response->getPayerEmail(), $subject, $body, $headers);
+            }
+        } else {
+            // receiver_id no coincide
+        }
+    } else {
+        // Usar versión anterior de la API de notificación
+    }
+} catch (\Khipu\ApiException $exception) {
+    print_r($exception->getResponseObject());
+}
--- validate_payment.php
+++ validate_payment.php
@@ -5,18 +5,19 @@ $configuration = new Khipu\Configuration();
 $configuration->setSecret(trim(file_get_contents("../SECRET")));
 $configuration->setReceiverId(trim(file_get_contents("../RECEIVER_ID")));
 $configuration->setPlatform('demo-client', '2.0');
+$notificationToken = trim(file_get_contents("../NOTIFICATION_TOKEN"));
 # $configuration->setDebug(true);
 $client = new Khipu\ApiClient($configuration);
 $payments = new Khipu\Client\PaymentsApi($client);
 
 try {
-    $ops = array(
-        "notify_url" => "https://culturaparatodos.herokuapp.com/demo-notify-js.php",
-        "transaction_id" => "TX-1234",
-    );
-    $response = $payments->paymentsPost('Pago de demo', 'CLP', 1000, $ops);
+    $response = $payments->paymentsGet($notificationToken);
 
     print "PAYMENT_ID: " . $response->getPaymentId() . "\n";
+    print "TRANSACTION_ID: " . $response->getTransactionId() . "\n";
+    print "AMOUNT: " . $response->getAmount() . "\n";
+    print "CURRENCY: " . $response->getCurrency() . "\n";
+    print "STATUS: " . $response->getStatus() . "\n";
 
 } catch (Exception $e) {
     echo $e->getMessage();
