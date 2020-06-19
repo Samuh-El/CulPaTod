03649dbcc341606e27751d844c28a79133c89e4d
= <samuelgajardo@outlook.com>
2020-06-18 22:00:06 -0400

    asd

--- demo-notify-js.php
+++ demo-notify-js.php
@@ -19,6 +19,30 @@ try {
         // $configuration->getSecret();
 
         file_put_contents("php://stderr", (string)$notification_token.PHP_EOL);
+
+        $servername = "190.107.177.34";
+        $username = "producto_Samuel";
+        $password = "S@muel01";
+        $dbname = "producto_chile";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = `INSERT into usuario (NombreUsuario,ClaveUsuario,direccion,celular,correo) VALUES (
+    'ASD','ASD','ASD',123,'` . (string)$notification_token . `')`;
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
         $client = new Khipu\ApiClient($configuration);
         $payments = new Khipu\Client\PaymentsApi($client);
 
