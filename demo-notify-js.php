aaafc4c2c2b9775412cf66c74a81ad6bbecf0693
= <samuelgajardo@outlook.com>
2020-06-18 23:54:32 -0400

    ajsd

--- demo-notify-js.php
+++ demo-notify-js.php
@@ -8,12 +8,11 @@ heroku logs --app 'nombre_de_aplicacion'*/
 
 $api_version = $_POST['api_version'];  // Parámetro api_version
 $notification_token = $_POST['notification_token']; //Parámetro notification_token
-
 $paymentId = $_GET['id']; // Obtener el id de pago generado
 
 // GET -> Query Params -> www.hola.com/hola?queryParam=valor
-// POST -> ??? Body -> 
-// REQUEST -> ???+-
+// POST -> Body -> 
+// REQUEST -> ???
 
 $amount = 5000;
 // file_put_contents("php://stderr", "sending push !!!".PHP_EOL);
@@ -36,8 +35,7 @@ try {
         $response = $payments->paymentsGet($notification_token);
         
         // Imprime todos los valores del token:
-        //file_put_contents("php://stderr", "Fuera del FOREACH: " . (string)$response.PHP_EOL);
-        
+        file_put_contents("php://stderr", (string)$response.PHP_EOL);
         foreach ($response as $key => $value) {
             file_put_contents("php://stderr", (string)$key.PHP_EOL);
             file_put_contents("php://stderr", (string)$value.PHP_EOL);
@@ -47,8 +45,22 @@ try {
             if ($response->getStatus() == 'done'
              && $response->getAmount() == $amount
             ) {
-                file_put_contents("php://stderr", "PASO EL DONE ".PHP_EOL);
+                // file_put_contents("php://stderr", "paso message !!!".PHP_EOL);
+                // $headers = 'From: "Comercio de prueba" <no-responder@khipu.com>' . "\r\n";
+                // $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+                // $subject = 'La compra de prueba funciona';
+                // $body = <<<EOF
+                // Hola<br/>
+                // <br/>
+                // <p>
+                // Recibes este correo pues el pago de prueba fue conciliado por khipu
+                // </p>
 
+                // EOF;
+
+                //file_put_contents("php://stderr", "paso email params!!!".PHP_EOL);
+                //mail($response->getPayerEmail(), $subject, $body, $headers);
+                //file_put_contents("php://stderr", "paso email sended!!!".PHP_EOL);
             }
         } else {
             // receiver_id no coincide
